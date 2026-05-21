6fa18c926a5c7f3b50ebacafc2985a7ee4056ff6
lucas <lucas.hamacher@ma-versand.de>
2026-05-21 13:02:01 +0200

    add: install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
