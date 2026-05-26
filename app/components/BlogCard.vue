12ab013eac8bdccab32e30aa554f81c7cacac454
lucas <lucashamacher01@gmail.com>
2026-05-26 16:04:55 +0200

    add: tag list in BlogCard component

--- app/components/BlogCard.vue
+++ app/components/BlogCard.vue
@@ -5,6 +5,7 @@ defineProps<{
     title?: string
     description?: string
     date?: string
+    tags?: string[]
   }
 }>()
 </script>
@@ -22,8 +23,10 @@ defineProps<{
       {{ post.title }}
     </h2>
 
-    <p class="mt-2 text-zinc-400">
+    <p class="mt-2 mb-4 text-zinc-400">
       {{ post.description }}
     </p>
+
+    <span v-for="tag in post.tags" :key="tag" class="mr-2 inline-flex items-center justify text-white py-1 px-4 rounded-full bg-cyan-700">{{ tag }}</span>
   </NuxtLink>
 </template>
